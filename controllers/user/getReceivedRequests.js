13b20aa0bc72af8182140724207eddc3f3bd9859
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-27 18:52:51 +0530

    Create getReceivedRequests user controller

---
+++ controllers/user/getReceivedRequests.js
@@ -0,0 +1,26 @@
+import asyncHandler from "../../utils/asyncHandler.js";
+
+/********************************************************
+ * @GET_RECEIVED_REQUESTS
+ * @METHOD GET
+ * @route /api/user/request/received
+ * @description Check for req.user and get the list of friend received requests
+ * @parameters none
+ * @return receivedRequests object
+ *********************************************************/
+
+const getReceivedRequests = asyncHandler(async (req, res) => {
+  const { user } = req;
+
+  const populatedUser = await user
+    .populate("receivedFriendRequests", "name profilePicture")
+    .execPopulate();
+  const receivedRequests = populatedUser.receivedFriendRequests;
+
+  res.status(200).json({
+    success: true,
+    receivedRequests,
+  });
+});
+
+export default getReceivedRequests;
