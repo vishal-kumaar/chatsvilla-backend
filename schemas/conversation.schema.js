dadd4504415501f1c10e45e68d769152315a6af7
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-27 14:02:19 +0530

    Create conversation schema

---
+++ schemas/conversation.schema.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const conversationSchema = new mongoose.Schema(
+  {
+    participants: [
+      {
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+          required: true,
+        },
+        unreadMessageCount: {
+          type: Number,
+          default: 0,
+        },
+      },
+    ],
+    messages: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Message",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Conversation", conversationSchema);
