c48a1a7e2cbd80f34a87bd21f0f8995cf3333072
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-27 19:05:57 +0530

    Create getSentRequests user controller

---
+++ controllers/user/getSentRequests.js
@@ -0,0 +1,26 @@
+import asyncHandler from "../../utils/asyncHandler.js";
+
+/********************************************************
+ * @GET_SENT_REQUESTS
+ * @METHOD GET
+ * @route /api/user/request/sent
+ * @description Check for req.user and get the list of friend sent requests
+ * @parameters none
+ * @return sentRequests object
+ *********************************************************/
+
+const sentFriendRequests = asyncHandler(async (req, res) => {
+  const { user } = req;
+
+  const populatedUser = await user
+    .populate("sentFriendRequests", "name profilePicture")
+    .execPopulate();
+  const sentRequests = populatedUser.sentFriendRequests;
+
+  res.status(200).json({
+    success: true,
+    sentRequests,
+  });
+});
+
+export default sentFriendRequests;
