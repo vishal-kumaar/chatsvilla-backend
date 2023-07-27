b60fb09130890f0346942caf3971e020d746865d
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-27 18:30:10 +0530

    Create getFriendsById user controller

---
+++ controllers/user/getFriendsById.js
@@ -0,0 +1,34 @@
+import asyncHandler from "../../utils/asyncHandler.js";
+import CustomError from "../../utils/CustomError.js";
+import User from "../../schemas/user.schema.js";
+
+/********************************************************
+ * @GET_FRIENDS_BY_ID
+ * @METHOD GET
+ * @route /api/user/friends:userId
+ * @description get friends by id controller to get the friends list by user id
+ * @parameters userId
+ * @return friends object
+ *********************************************************/
+
+const getFriendsId = asyncHandler(async (req, res) => {
+  const { userId } = req.params;
+
+  const user = await User.findById(userId);
+
+  if (!user) {
+    throw new CustomError("User not found", 404);
+  }
+
+  const populatedUser = await user
+    .populate("friends", "name profilePicture")
+    .execPopulate();
+  const friends = populatedUser.friends;
+
+  res.status(200).json({
+    success: true,
+    friends,
+  });
+});
+
+export default getFriendsId;
