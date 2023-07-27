46716bb437e433c52cdda4d43d662acbfdcf54fd
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-27 13:17:17 +0530

    Create user schema

---
+++ schemas/user.schema.js
@@ -0,0 +1,104 @@
+import mongoose from "mongoose";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    profilePictrue: {
+      type: String,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    bio: {
+      type: String,
+      maxlength: [200, "Bio at most 200 characters long"],
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: [8, "Password must have at least 8 characters"],
+    },
+    friends: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    sentFriendRequests: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    receivedFriendRequests: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    online: {
+      type: Boolean,
+      default: false,
+    },
+    lastSeen: {
+      type: Date,
+    },
+    blockedUsers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcryptjs.hash(this.password, 10);
+  }
+  next();
+});
+
+userSchema.methods = {
+  compatePassword: async function (enteredPassword) {
+    return await bcryptjs.compare(enteredPassword, this.password);
+  },
+
+  getUserToken: async function () {
+    return jwt.sign(
+      {
+        id: this._id,
+        username: this.username,
+      },
+      process.env.JWT_SECRET_KEY,
+      {
+        expiresIn: process.env.JWT_EXPIRY,
+      }
+    );
+  },
+
+  isBlocked: function (userid) {
+    return this.blockedUsers.includes(userid);
+  },
+};
+
+export default mongoose.model("User", userSchema);
