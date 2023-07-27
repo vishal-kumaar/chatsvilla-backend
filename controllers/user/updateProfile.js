5bf5f46c0a66f87071da5dd61def2bdc5d1acd7e
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-27 19:57:03 +0530

    Create updateProfile user controller

---
+++ controllers/user/updateProfile.js
@@ -0,0 +1,50 @@
+import asyncHandler from "../../utils/asyncHandler.js";
+import CustomError from "../../utils/CustomError.js";
+import User from "../../schemas/user.schema.js";
+import { genderEnum } from "../../utils/enums.js";
+
+/********************************************************
+ * @UPDATE_PROFIlE
+ * @METHOD PATCH
+ * @route /api/user/update
+ * @description Check for req.user and update the user profile
+ * @parameters name, username, gender, bio
+ * @return user object
+ *********************************************************/
+
+const updateProfile = asyncHandler(async (req, res) => {
+  const { user } = req;
+  const { name, username, gender, bio } = req.body;
+
+  if (!name || !username) {
+    throw new CustomError("Name and username are required", 400);
+  }
+
+  const existingUsername = await User.findOne({
+    username,
+    _id: { $ne: user._id },
+  });
+
+  if (existingUsername) {
+    throw new CustomError(
+      "Username already exists, Try different username",
+      409
+    );
+  }
+
+  user.name = name;
+  user.username = username;
+  if (genderEnum.includes(gender)) {
+    user.gender = gender;
+  }
+  user.bio = bio;
+
+  await user.save();
+
+  res.status(200).json({
+    success: true,
+    message: "Profile Update Successfully",
+  });
+});
+
+export default updateDetails;
