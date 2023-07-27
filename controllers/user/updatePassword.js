a184b8b4f560f9edfb11069ea51ff872a6ad0e78
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-27 20:19:57 +0530

    Create updatePassword user controller

---
+++ controllers/user/updatePassword.js
@@ -0,0 +1,46 @@
+import asyncHandler from "../../utils/asyncHandler.js";
+import CustomError from "../../utils/CustomError.js";
+import User from "../../schemas/user.schema.js";
+import { genderEnum } from "../../utils/enums.js";
+
+/********************************************************
+ * @UPDATE_PASSWORD
+ * @METHOD PATCH
+ * @route /api/user/password/update
+ * @description Check for req.user and update the user password
+ * @parameters old password, new password and confirm new password
+ * @return user object
+ *********************************************************/
+
+const updatePassword = asyncHandler(async (req, res) => {
+  const { oldPassword, newPassword, confirmNewPassword } = req.body;
+
+  if (!oldPassword || !newPassword || !confirmNewPassword) {
+    throw new CustomError("All fields are required", 400);
+  }
+
+  if (newPassword !== confirmNewPassword) {
+    throw new CustomError("New and Confirm Password both must be same", 400);
+  }
+
+  const user = await User.findById(req.user._id).select("+password");
+
+  const isPasswordMatch = await user.comparePassword(oldPassword);
+
+  if (!isPasswordMatch) {
+    throw new CustomError("Incorrect old password", 400);
+  }
+
+  user.password = newPassword;
+  await user.save();
+
+  user.password = undefined;
+
+  res.status(200).json({
+    success: true,
+    message: "Profile Update Successfully",
+    user,
+  });
+});
+
+export default updatePassword;
