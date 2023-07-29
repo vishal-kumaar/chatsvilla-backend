ab59d2cc2a7c70e346f85340047805db1dbb8c76
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-29 18:35:55 +0530

    Update sendMessage message controller

--- controllers/message/sendMessage.js
+++ controllers/message/sendMessage.js
@@ -2,12 +2,15 @@ import asyncHandler from "../../utils/asyncHandler.js";
 import CustomError from "../../utils/CustomError.js";
 import Conversation from "../../schemas/conversation.schema.js";
 import Message from "../../schemas/message.schema.js";
+import User from "../../schemas/user.schema.js";
+import { io, connectedUsers } from "../../app.js";
+import { messageTypeEnum } from "../../utils/enums.js";
 
 /********************************************************
  * @SEND_MESSAGE
  * @METHOD POST
  * @route /api/conversations/:conversationId/messages
- * @description Send a new message to a conversation
+ * @description Send a new message to an existing conversation or create a new conversation and send the message
  * @parameters conversationId (in URL)
  * @body message (in request body)
  * @return sent message object
@@ -15,44 +18,81 @@ import Message from "../../schemas/message.schema.js";
 
 const sendMessage = asyncHandler(async (req, res) => {
   const { conversationId } = req.params;
-  const { message } = req.body;
-  const senderId = req.user._id;
+  const { message, messageType } = req.body;
+  const { user } = req;
 
   if (!message) {
     throw new CustomError("Message is required", 400);
   }
 
-  const conversation = await Conversation.findById(conversationId);
+  if (!messageTypeEnum.includes(messageType)) {
+    throw new CustomError("Message type is unsupported", 404);
+  }
+
+  let conversation = await Conversation.findById(conversationId).populate(
+    "participants.user"
+  );
 
   if (!conversation) {
-    throw new CustomError("Conversation not found", 404);
-  }
+    const recipientId = conversationId;
 
-  if (
-    !conversation.participants.some((participant) =>
-      participant.user.equals(senderId)
-    )
-  ) {
-    throw new CustomError(
-      "You are not a participant in this conversation",
-      403
+    const recipientUser = await User.findById(recipientId);
+    if (!recipientUser) {
+      throw new CustomError("Invalid conversationId/recipientId", 404);
+    }
+
+    conversation = await Conversation.findOne({
+      type: "Individual",
+      $and: [
+        { "participants.user": user._id },
+        { "participants.user": recipientId },
+      ],
+    }).exec();
+
+    if (!conversation) {
+      if (!req.user.friends.includes(recipientId)) {
+        throw new CustomError(
+          "You can only send messages to your friends. Please request a chat with the recipient.",
+          403
+        );
+      }
+
+      conversation = await Conversation.create({
+        participants: [{ user: user._id }, { user: recipientId }],
+        type: "individual",
+      });
+    }
+  } else {
+    const isParticipant = conversation.participants.some(
+      (participant) => participant.user._id.toString() === user._id.toString()
     );
+
+    if (!isParticipant) {
+      throw new CustomError(
+        "You are not a participant in this conversation",
+        403
+      );
+    }
   }
 
-  const newMessage = new Message({
-    sender: senderId,
-    recipients: conversation.participants.map((participant) => ({
-      user: participant.user,
-    })),
-    conversation: conversationId,
+  const recipientIds = conversation.participants.map(
+    (participant) => participant.user
+  );
+
+  const newMessage = await Message.create({
+    sender: user._id,
+    recipients: recipientIds,
+    conversation: conversation._id,
     message,
+    messageType,
   });
 
-  await newMessage.save();
-
-  conversation.messages.push(newMessage._id);
-  conversation.lastMessage = newMessage._id;
-  await conversation.save();
+  conversation.participants.forEach((participant) => {
+    const recipientId = participant.user._id;
+    if (connectedUsers.includes(recipientId.toString())) {
+      io.to(recipientId).emit("newMessage", newMessage);
+    }
+  });
 
   res.status(201).json({
     success: true,
