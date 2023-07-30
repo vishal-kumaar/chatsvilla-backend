8ee584357acd0f3fe7d5f500edf217869bb6ef27
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-30 09:57:40 +0530

    Create isLoggedIn middleware

---
+++ middlewares/IsLoggedIn.js
@@ -0,0 +1,23 @@
+import asyncHandler from "../utils/asyncHandler.js";
+import CustomError from "../utils/CustomError.js";
+import User from "../schemas/user.schema.js";
+import jwt from "jsonwebtoken";
+import config from "../config/index.js";
+
+const isLoggedIn = asyncHandler(async (req, _res, next) => {
+  const token = req.header("Authorization")?.replace("Bearer ", "");
+
+  if (!token) {
+    throw new CustomError("Not authorized to access this route", 401);
+  }
+
+  try {
+    const decodedJwtPayload = jwt.verify(token, config.jwtSecret);
+    req.user = await User.findById(decodedJwtPayload._id);
+    next();
+  } catch (error) {
+    throw new CustomError("Session expired! Login to continue", 401);
+  }
+});
+
+export default isLoggedIn;
