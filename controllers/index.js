48bf84291174bbb4432e43dfe2cb07999c93abb4
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-30 10:53:34 +0530

    Add sendRequest user controller

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,7 @@ import signup from "./auth/signup.js";
 import login from "./auth/login.js";
 import getProfileById from "./user/getProfileById.js";
 import getFriendsById from "./user/getFriendsById.js";
+import sendRequest from "./user/sendRequest.js";
 import getSentRequests from "./user/getSentRequests.js";
 import getReceivedRequests from "./user/getReceivedRequests.js";
 import updateProfile from "./user/updateProfile.js";
@@ -19,6 +20,7 @@ const controllers = {
   login,
   getProfileById,
   getFriendsById,
+  sendRequest,
   getSentRequests,
   getReceivedRequests,
   updateProfile,
