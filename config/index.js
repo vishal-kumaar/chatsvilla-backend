573748e80ee60dcfe7221558c9accceb027db82d
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-30 11:57:18 +0530

    Update config

--- config/index.js
+++ config/index.js
@@ -5,6 +5,9 @@ dotenv.config();
 const config = {
   jwtSecret: process.env.JWT_SECRET_KEY,
   jwtExpiry: process.env.JWT_EXPIRY,
+  allowedOrigins: process.env.ALLOWED_ORIGINS.split(","),
+  mongoDbUri: process.env.MONGODB_URI,
+  port: process.env.PORT,
 };
 
 export default config;
