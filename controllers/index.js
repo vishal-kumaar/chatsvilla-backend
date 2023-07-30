33803dc611c51e2827800b0a1efffcfcf0bbddac
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-30 10:19:55 +0530

    Create index.js for controllers

---
+++ controllers/index.js
@@ -0,0 +1,35 @@
+import signup from "./auth/signup.js";
+import login from "./auth/login.js";
+import getProfileById from "./user/getProfileById.js";
+import getFriendsById from "./user/getFriendsById.js";
+import getSentRequests from "./user/getSentRequests.js";
+import getReceivedRequests from "./user/getReceivedRequests.js";
+import updateProfile from "./user/updateProfile.js";
+import updateStatus from "./user/updateStatus.js";
+import updatePassword from "./user/updatePassword.js";
+import sendMessage from "./message/sendMessage.js";
+import deleteMessage from "./message/deleteMessage.js";
+import getConversation from "./conversation/getConversation.js";
+import getAllConversations from "./conversation/getAllConversation.js";
+import markConversationAsRead from "./conversation/markConversationAsRead.js";
+import deleteConversation from "./conversation/deleteConversation.js";
+
+const controllers = {
+  signup,
+  login,
+  getProfileById,
+  getFriendsById,
+  getSentRequests,
+  getReceivedRequests,
+  updateProfile,
+  updateStatus,
+  updatePassword,
+  sendMessage,
+  deleteMessage,
+  getConversation,
+  getAllConversations,
+  markConversationAsRead,
+  deleteConversation,
+};
+
+export default controllers;
