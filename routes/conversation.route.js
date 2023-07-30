877fc47473115174575c1b524abac9be322602c0
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-30 11:14:40 +0530

    Create conversation route

---
+++ routes/conversation.route.js
@@ -0,0 +1,34 @@
+import express from "express";
+import controllers from "../controllers/index.js";
+import middlewares from "../middlewares/index.js";
+
+const router = express.Router();
+
+router.post(
+  "/:conversationId/message",
+  [middlewares.isLoggedIn, middlewares.isValidMessageType],
+  controllers.sendMessage
+);
+router.delete(
+  "/message/:messageId",
+  middlewares.isLoggedIn,
+  controllers.deleteMessage
+);
+router.get(
+  "/:conversationId",
+  middlewares.isLoggedIn,
+  controllers.getConversation
+);
+router.get("/", middlewares.isLoggedIn, controllers.getAllConversations);
+router.patch(
+  "/:conversationId/read",
+  middlewares.isLoggedIn,
+  controllers.markConversationAsRead
+);
+router.delete(
+  "/:conversationId",
+  middlewares.isLoggedIn,
+  controllers.deleteConversation
+);
+
+export default router;
